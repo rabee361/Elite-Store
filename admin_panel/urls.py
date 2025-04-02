55a29d91bcb7c343a5e0e7b3ff3f0fbb2d3b0686
rabee361 <rha60540@gmail.com>
2025-04-02 16:33:25 +0300

    fix

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('login/' , users_views.LoginView.as_view() , name="login"),
     path('logout/' , users_views.LogoutView.as_view() , name="logout"),
     path('' , users_views.DashboardView.as_view() , name="dashboard"),
-    path('partial/' , users_views.DashboardPartialView.as_view() , name="dashboard-partial"),]
+    path('partial/' , users_views.DashboardPartialView.as_view() , name="dashboard-partial"),
+    path('catalogs/' , users_views.ListCatalogsView.as_view() , name="catalogs"),
+    ]
     
--- admin_panel/views/users_views.py
+++ admin_panel/views/users_views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
+from utils.views import CustomListBaseView
 
 # Create your views here.
 login_required_m =  method_decorator(login_required(login_url='login') , name="dispatch")
@@ -65,3 +66,30 @@ class DashboardPartialView(View):
         return render(request, 'admin_panel/partials/dashboard_partial.html')
 
 
+# @login_required_m
+# class ListCatalogsView(CustomListBaseView):
+#     # model = Catalog
+#     context_object_name = 'catalogs'
+#     context_fields = ['id','catalog_type','organization','visits','short_url']
+#     template_name = 'admin_panel/organization/catalogs/catalogs.html' 
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         for catalog in context['catalogs']:
+#             catalog.short_url = self.request.build_absolute_uri(catalog.get_absolute_url())
+#         return context
+
+#     def get_queryset(self):
+#         queryset = super().get_queryset().select_related('organization')
+#         q = self.request.GET.get('q', '')
+#         if self.request.htmx:
+#             self.template_name = 'admin_panel/partials/catalogs_partial.html'
+#         if q:
+#             return queryset.filter(organization__name__icontains=q)
+#         else:
+#             return queryset
+
+
+class ListCatalogsView(View):
+    def get(self, request):
+        return render(request, 'admin_panel/catalogs/catalogs.html',context={})
